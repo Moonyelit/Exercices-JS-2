b5bde29beecf7c02b6332b14e7641c52f6ca9fad
moonye@live.fr <moonye@live.fr>
2024-11-18 13:57:50 +0100

    ADD : EXO 2

---
+++ Exercice_2/script.js
@@ -0,0 +1,8 @@
+let formThanks = document.querySelector('form');
+
+formThanks.addEventListener('focusout' , function getFocus  () {
+    alert("Merci de votre participation")
+});
+
+console.log(formThanks)
+
